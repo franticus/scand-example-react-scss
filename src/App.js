1ebd9fd07fc1bd04fbc2b1405dece3f18b4e34b4
S.Zelensky <zelenstein@gmail.com>
2023-04-10 15:41:19 +0300

    scand: init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        123
+      </header>
+    </div>
+  );
+}
+
+export default App;
