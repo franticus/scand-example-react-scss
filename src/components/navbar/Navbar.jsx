8b75d59cde4fa2e5b6280b0243589905ccfc5535
S.Zelensky <zelenstein@gmail.com>
2023-04-11 16:17:18 +0300

    scand: burger

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Logo, Button } from '../common';
 import s from './Navbar.module.scss';
+import burgerIcon from './img/burger.svg'
 
 export const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const burger = useRef();
+  const menu = useRef();
+
+  const menuOpenHandler = () => {
+    setMenuOpen(!isMenuOpen);
+    console.log(isMenuOpen);
+    burger.current.style.transform =
+      isMenuOpen
+        ? 'rotate(0deg)'
+        : 'rotate(90deg)';
+    menu.current.style.transform =
+      isMenuOpen
+        ? 'translate(100%)'
+        : 'translate(-100%)';
+    menu.current.style.opacity =
+      isMenuOpen
+        ? '0'
+        : '1';
+  }
+
   return (
     <div className={s.navbar}>
       <Logo />
@@ -11,6 +33,15 @@ export const Navbar = () => {
         <a className={s.link} href='/'>Services</a>
         <a className={s.link} href='/'>About</a>
         <Button color="ghost" size="sm" text="Log in" />
+      </div>
+      <div className={s.burger} onClick={() => menuOpenHandler()}>
+        <img className={s.menuIcon} src={burgerIcon} alt="menu" ref={burger}></img>
+        <div className={s.burger__links} ref={menu}>
+          <a className={s.burger__link} href='/'>Product</a>
+          <a className={s.burger__link} href='/'>Services</a>
+          <a className={s.burger__link} href='/'>About</a>
+          <Button color="ghost" size="sm" text="Log in" />
+        </div>
       </div>
     </div>
   )
